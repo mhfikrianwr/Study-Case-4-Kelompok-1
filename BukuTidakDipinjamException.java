e9b716a9d00999c11b0ef483a0a47d490201112c
Muhammad Fikri A <fikrianwar9345@gmail.com>
2025-03-27 15:24:44 +0700

    Final Update

--- BukuTidakDipinjamException.java
+++ BukuTidakDipinjamException.java
@@ -1,5 +1,5 @@
 public class BukuTidakDipinjamException extends RuntimeException {
     public BukuTidakDipinjamException(String judulBuku) {
-        super("Buku dengan judul '" + judulBuku + "' tidak dipinjam oleh anggota ini.");
+        super("ERROR EXCEPTION : Buku dengan judul '" + judulBuku + "' tidak dipinjam oleh anggota ini.");
     }
 }
