f8253fac2b205b6d49c59732b79e7a881bae3005
Muhammad Fikri A <fikrianwar9345@gmail.com>
2025-03-27 15:27:13 +0700

    Final Update

--- Perpustakaan.java
+++ Perpustakaan.java
@@ -12,31 +12,20 @@ public class Perpustakaan{
     }
     public void pinjamBuku(String idAnggota, String judul){
         Anggota anggota = daftarAnggota.get(idAnggota);
-        Buku buku = daftarBuku.get(0);
-        boolean foundAnggota = false;
-        for(String i : daftarAnggota.keySet()){
-            if(i.equalsIgnoreCase(idAnggota)){
-                anggota = daftarAnggota.get(i);
-                foundAnggota = true;
+        if (anggota == null) throw new AnggotaTidakDitemukanException(idAnggota);
+
+        Buku bukuDitemukan = null;
+        for (Buku buku : daftarBuku) {
+            if (buku.judul.equalsIgnoreCase(judul) && buku.tersedia) {
+                bukuDitemukan = buku;
+                break;
             }
         }
-        if(!foundAnggota) throw new AnggotaTidakDitemukanException("Anggota tidak ditemukan !!! idAnggota tidak valid, tidak dapat meminjam buku");
-        else{
-            boolean foundBuku = false;
-             for(Buku a : daftarBuku){
-                if(a.judul.equalsIgnoreCase(judul)){
-                    buku = a;
-                    foundBuku = true;
-                    break;
-                }
-            }
-            if(!foundBuku) throw new BukuTidakTersediaException(judul);
-            else{
-                anggota.daftarBukuDipinjam.add(buku);
-                buku.tersedia = true;
-            }
-        }
-    
+        if (bukuDitemukan == null) throw new BukuTidakTersediaException(judul);
+
+        anggota.daftarBukuDipinjam.add(bukuDitemukan);
+        bukuDitemukan.tersedia = false;
+        System.out.println("Anggota '" + anggota.nama + "' berhasil meminjam buku '" + judul + "'!");
     }
     public void kembaliBuku(String idAnggota, String judul){
         Anggota anggota = daftarAnggota.get(idAnggota);
@@ -48,25 +37,35 @@ public class Perpustakaan{
                 foundAnggota = true;
             }
         }
-        if(!foundAnggota) throw new AnggotaTidakDitemukanException("Anggota tidak ditemukan !!! idAnggota tidak valid, tidak dapat mengembalikan buku");
+        if(!foundAnggota) throw new AnggotaTidakDitemukanException(idAnggota);
         else{
             if(!anggota.daftarBukuDipinjam.contains(buku)){
                 throw new BukuTidakDipinjamException(buku.judul);
             }else{
-                System.out.println("Buku " + buku.judul + " berhasil dikembalikan!!!");
+                System.out.println(" Anggota dengan id " + idAnggota +" berhasil mengembalikan Buku " + buku.judul + " !!!");
                 anggota.daftarBukuDipinjam.remove(buku);
                 buku.tersedia = true;
+                System.out.println("\n");
             }
         }
     
     }
     public void searchBuku(String title){  
         boolean boolFound = false;
+        Buku a = daftarBuku.get(0);
         for(Buku buku : daftarBuku){
             if(buku.judul.equalsIgnoreCase(title)){
                 boolFound = true;
+                a = buku;
             }
         }
         if(!boolFound) throw new BookDoesntExist(title);
+        else{
+            System.out.println("Buku dengan judul " + title + " ditemukan");
+        }
+    }
+    public void searchAnggota(String idAnggota){
+        if(!daftarAnggota.containsKey(idAnggota)) throw new AnggotaTidakDitemukanException(idAnggota);
+        else System.out.println("Anggota dengan id " + idAnggota + " ditemukan !!!");
     }
 }
